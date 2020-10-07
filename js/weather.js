5576b26ec3357c786b2c555e45fb29a1ed180762
Kamon <ninetail9721@gmail.com>
2020-10-07 20:41:30 +0900

    Add weather icon

--- js/weather.js
+++ js/weather.js
@@ -2,11 +2,17 @@ const WEATHER_API_KEY = "767cb8dd7684576b6e1bfd9dedd24332";
 const WEATHER_API = "https://api.openweathermap.org/data/2.5/weather?";
 
 const weather = document.querySelector(".js-weather .weather__text");
+const icon = document.querySelector(".weather-state__icon");
+const state_text = document.querySelector(".wather-state__text");
 
 function paintWeather(weatherObj) {
   weather.innerHTML = `${Math.floor(weatherObj.temperature)}° @ ${
     weatherObj.name
   }`;
+  const wi = document.createElement("i");
+  wi.classList.add("wi", `wi-owm-${weatherObj.stateId}`);
+  icon.appendChild(wi);
+  state_text.innerHTML = weatherObj.state;
 }
 
 function getWeather(coords) {
@@ -18,11 +24,14 @@ function getWeather(coords) {
     .then((json) => {
       const weatherObj = {
         name: json.name,
+        state: json.weather[0].main,
+        stateId: json.weather[0].id,
         temperature: json.main.temp,
         exHour: expirationHour,
       };
       localStorage.setItem("weathers", JSON.stringify(weatherObj));
       paintWeather(weatherObj);
+      console.log(json.weather[0].main);
     });
 }
 
@@ -43,10 +52,10 @@ function getFailCoords() {
 
 function init() {
   const currentCoords = localStorage.getItem("coords");
-  if (currentCoords === null) {
+  const weatherObj = localStorage.getItem("weathers");
+  if (currentCoords === null || weatherObj === null) {
     navigator.geolocation.getCurrentPosition(getCoords, getFailCoords);
   } else {
-    const weatherObj = localStorage.getItem("weathers");
     const parsedweather = JSON.parse(weatherObj);
     const todayHour = new Date();
     const parsedhour = new Date(parsedweather.exHour);
